5b5e9e73017468b77e657a5fa3dffa2bfbe8fcc5
mrbeastmahmoud <mahmoudsu22@gmail.com>
2022-03-19 12:33:31 +0200

    Location selector  component

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const style = {
   wrapper: `h-screen w-screen flex flex-col`,
   main: `h-full w-screen flex-1 z-10`,
   mapContainer: `flex-1 w-full h-full`,
-  rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
+  rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-12 -left-4  flex flex-col justify-end z-20`,
   rideRequest: `h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll`,
 };
 export default function Home() {
